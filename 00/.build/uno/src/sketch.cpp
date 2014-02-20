86dc0e4a29a922622755371e5768f57284396520
gskielian <gregory.kielian@gmail.com>
2014-02-20 12:51:34 -0800

    reorganized into 00 folder

---
+++ 00/.build/uno/src/sketch.cpp
@@ -0,0 +1,35 @@
+#include <Arduino.h>
+#include <avr/io.h>
+void setup();
+void loop();
+#line 1 "src/sketch.ino"
+//#include <avr/io.h>
+
+void setup()
+{
+  DDRB = 0b00100001;  // Data Direction Register for port B
+  PORTB = 0b00000000; // setting high-low values
+
+
+
+  TCCR1B |= 1<<CS10;  // setting CS10 bit only, meaning no prescaling
+
+  while(1)
+  {
+      if (TCNT1 > 2232)
+      {
+        TCNT1 = 0;
+
+        PORTB ^= 0b00100001;  // xor is useful as it can toggle
+                              // PINB0 is a macro for the number of shifts in
+                              // order to write to PINB0
+      }
+        PORTB ^= 0b00100001;  // xor is useful as it can toggle
+  }
+
+}
+
+void loop()
+{
+  delay(1000);
+}
